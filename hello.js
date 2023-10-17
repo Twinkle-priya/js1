5c20b9227beb323bb2155d1224c3f6c23dc2260e
Twinkle-priya <priyatwinkle771@gmail.com>
2023-10-17 20:58:33 +0530

    fg

---
+++ hello.js
@@ -0,0 +1,73 @@
+
+//console.log("HELLO WORLD!!");
+
+// LOOPS
+//1). FOR LOOP 
+/*for( var i=0; i<5; i++){
+    let name= "john Doe";
+    console.log("Hi My Name is :- "+ name);
+}*/
+
+// if else loop 
+/*let age = 18;
+if(age>18){
+console.log("  You are Adult");
+}
+else{
+    console.log("You are Teenager");
+}*/
+
+//STRING concat
+/* str1 = 'Hello'
+str2 = ' world'
+str3 = str1.concat(str2)
+console.log(str3);*/
+
+//INDEX OF STRING
+/*str1 = ' Ram is a good boy'
+substr = 'good'
+ result = str1.indexOf(substr)
+console.log(result);*/
+
+//Switch case
+/*let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+console.log(day);*/
+
+//Operators 
+let a =  10;
+let b= 26;
+sum = a+b;
+console.log("sum =" ,sum);
+Diff = a-b;
+console.log( "Diff =" ,Diff);
+division = a/b;
+console.log( "division =",division);
+Multiplication = a*b;
+console.log( "Multiplication =",Multiplication);
+
+
+
+
+
