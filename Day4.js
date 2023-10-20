c0f2091ce58b247c7d0fce8bf43aa7369ec8055e
Twinkle-priya <priyatwinkle771@gmail.com>
2023-10-20 17:46:45 +0530

    Day4

---
+++ Day4.js
@@ -0,0 +1,27 @@
+// TO PRINT THE LENGTH OF THE STRING
+let name = "twinkle"
+console.log(name.length)
+
+// TO UPPERCASE METHOD
+ console.log(name.toUpperCase())
+ 
+ //TO LOWERCASE METHOD 
+  console.log(name.toLowerCase())
+
+  //slice method
+  console.log(name.slice(0,4))
+
+  // Replace method
+  let name1 = "Twinkle Priya"
+  let name2= (name1.replace("Priya","Sharma"))
+  console.log(name2)
+
+  // to print the name using index
+  let n1 = "Twinkle"
+  console.log(n1[0])
+  console.log(n1[1])
+  console.log(n1[2])
+  console.log(n1[3])
+  console.log(n1[4])
+  console.log(n1[5])
+  console.log(n1[6])
